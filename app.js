7f1b21ba292764834adbb5ddc5d21ee513af39d0
Houyi Fan <fanosu.685@gmail.com>
2018-09-27 14:54:43 +0000

    add mlab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb://Fan:Fhysjk1@ds117423.mlab.com:17423/yelpcamp", { useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
