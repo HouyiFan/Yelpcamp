158ba3e53ecc9c377d53a01fa3a03f6957953bae
Houyi Fan <fanosu.685@gmail.com>
2018-09-28 22:21:11 +0000

    change the style of navbar

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get("/", function(req, res){
 //  ===========
 //show register form
 router.get("/register", function(req, res) {
-   res.render("register", {page: 'register'});
+   res.render("register", {page: "register"});
 });
 
 //handle sign up logic
@@ -35,7 +35,7 @@ router.post("/register", function(req, res){
 
 //show login form
 router.get("/login", function(req, res) {
-   res.render("login", {message: req.flash("error"), page: 'login'});
+   res.render("login", {message: req.flash("error"), page: "login"});
 });
 
 //handling login logic
